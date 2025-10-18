60516dd814e20025f54b82cbb3968b2539a5ad72
Rishi Raj <pothanaboyina.rishi2023@vitstudent.ac.in>
2025-10-18 11:57:20 +0530

    Add visit counter functionality in q1.php
    Implement a visit counter using cookies.

--- Assignment10/q1.php
+++ Assignment10/q1.php
@@ -1,1 +1,17 @@
+<?php
+// save as visits.php
+$count = 1;
+if (isset($_COOKIE['visits'])) {
+    $count = intval($_COOKIE['visits']) + 1;
+}
+setcookie('visits', strval($count), time() + 3600, "/"); // expire in 1 hour
+?>
+<!doctype html>
+<html>
+<head><meta charset="utf-8"><title>Visit Counter</title></head>
+<body>
+  <h1>Welcome!</h1>
+  <p>You have visited this page <strong><?= htmlspecialchars($count) ?></strong> <?= $count === 1 ? "time" : "times" ?>.</p>
+</body>
+</html>
 
